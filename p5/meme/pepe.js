edf9cd37c798b0171e3dbeaa644002982e95a279
OhLouis <31419327+OhLouis@users.noreply.github.com>
2019-03-06 18:44:31 -0500

    Add files via upload

--- p5/meme/pepe.js
+++ p5/meme/pepe.js
@@ -1,24 +1,32 @@
 let img; // Declare variable 'img'.
+let img2; // Declare variable 'img'.
 var col= 300;
 var c= 50;
 var d= 30;
 function setup() {
-  createCanvas(1000, 625);
+  createCanvas(windowWidth, windowHeight);
   img = loadImage('images/nervous_pepe.png'); // Load the PEPE
+  img2 = loadImage('images/rare_nervous_pepe.png'); // Load the RARE PEPE
 }
 function draw() {
 
 //background color should change with mouseX
   col,c,d = mouseX/3;
 background (col,c,d);
-  // Displays the image at its actual size at point (0,10)
-  image(img, 0, 10);
 
+if(mouseIsPressed){
+image(img2, 0,130);
+}
+else {
+
+  // Displays the image at its actual size at point (0,10)
+  image(img, 0, 130);
+}
   //left eye
   push();
   fill(0);
   //pupils should move side to side
-  ellipse(map(mouseX,0, width, 200, 300),260, 30, 30);
+  ellipse(map(mouseX,0, width, 200, 300),380, 30, 30);
   //ellipse(300, 260, 30, 30);
   pop();
 
@@ -26,7 +34,7 @@ background (col,c,d);
   push();
   fill(0);
   //pupils should move side to side
-  ellipse(map(mouseX, 0, width, 400, 498),260, 30, 30);
+  ellipse(map(mouseX, 0, width, 400, 498),380, 30, 30);
   //ellipse(500, 260, 30, 30);
   pop();
 }
